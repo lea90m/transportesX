7b6acadabdd72d0d82f44eeb3c92c488dd823549
lea90m <leandro90m@gmail.com>
2021-12-16 12:46:52 -0300

    123

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -4,7 +4,7 @@ var novedadesModel = require('../../models/novedadesModels');
 
 router.get('/', async function(req,res,next){
     
-    var novedades = await novedadesModel.getNovedades();
+    var novedades = await novedadesModel.getNovedades(); //query
 
     res.render('admin/novedades',{
         layout:'admin/layout',
@@ -14,5 +14,39 @@ router.get('/', async function(req,res,next){
     });
 });
 
+// vista del formulario de agregar
+
+router.get('/agregar', function (req,res,next){
+    res.render('admin/agregar',{
+        layout:'admin/layout'
+    });
+});
+
+// da funcionamiento al boton guardar
+
+router.post('/agregar', async (req,res,next) => {
+    try{
+        console.log(req.body);
+
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "" ){
+            await novedadesModel.insertNovedades(req.body);
+            res.redirect('/admin/novedades');
+        }else{
+            res.render('admin/agregar',{
+                layout:'admin/layout',
+                error:true,
+                message:'Todos los campos son requeridos'
+            })
+        }
+
+    }catch (error){
+        console.log(error);
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error:true,
+            message: 'No se cargo la novedad'
+        })
+    }
+})
 
 module.exports = router;
--- routes/novedades.js
+++ routes/novedades.js
@@ -1,9 +1,39 @@
-var express = require ('express');
+var express = require('express');
 var router = express.Router();
+var novedadesModel = require("../models/NovedadesModels");
+// var cloudinary = require('cloudinary').v2;
 
-router.get('/', function (req,res,netx){
-    res.render('novedades',{ isNovedades: true }) //novedades.hbs
+router.get("/", async function(res, res, next){
+
+    var novedades = await novedadesModel.getNovedades();
+    
+    novedades = novedades.map(novedad =>{
+        if(novedad.img_id){
+            const imagen = cloudinary.url(novedad.img_id, {
+                width: 960,
+                height: 200,
+                crop: 'fill'
+            });
+            return{
+                ...novedad,
+                imagen
+            }
+        }
+        else{
+            return{
+                ...novedad,
+                imagen:''
+            }
+        }
+    });
+    
+    res.render("novedades", {isNovedades: true, novedades});
+    
+
+
 })
+
+
 
 
 module.exports = router;
