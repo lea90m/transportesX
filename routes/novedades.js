d39978a1e69336f4f223826a33de8c1cad389b1a
lea90m <leandro90m@gmail.com>
2021-12-16 17:11:53 -0300

    novedades+ login

--- routes/novedades.js
+++ routes/novedades.js
@@ -1,33 +1,36 @@
 var express = require('express');
 var router = express.Router();
-var novedadesModel = require("../models/NovedadesModels");
+var novedadesModel = require('../models/NovedadesModels');
 // var cloudinary = require('cloudinary').v2;
 
-router.get("/", async function(res, res, next){
+router.get('/', async function(res, res, next){
 
     var novedades = await novedadesModel.getNovedades();
     
-    novedades = novedades.map(novedad =>{
-        if(novedad.img_id){
-            const imagen = cloudinary.url(novedad.img_id, {
-                width: 960,
-                height: 200,
-                crop: 'fill'
-            });
-            return{
-                ...novedad,
-                imagen
-            }
-        }
-        else{
-            return{
-                ...novedad,
-                imagen:''
-            }
-        }
-    });
+    // novedades = novedades.map(novedad =>{
+    //     if(novedad.img_id){
+    //         const imagen = cloudinary.url(novedad.img_id, {
+    //             width: 960,
+    //             height: 200,
+    //             crop: 'fill'
+    //         });
+    //         return{
+    //             ...novedad,
+    //             imagen
+    //         }
+    //     }
+    //     else{
+    //         return{
+    //             ...novedad,
+    //             imagen:''
+    //         }
+    //     }
+    // });
     
-    res.render("novedades", {isNovedades: true, novedades});
+    res.render('novedades', {
+        isNovedades:true, 
+        novedades
+    })
     
 
 
